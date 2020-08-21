1f957bb62a043387ca2c1632b7f44f6f897753dc
madoht <oht222@naver.com>
2020-08-22 03:02:20 +0900

    day.12

--- rooms/views.py
+++ rooms/views.py
@@ -1,13 +1,22 @@
-from math import ceil
+from django.views.generic import ListView
+from django.http import Http404
 from django.shortcuts import render
-from django.core.paginator import Paginator
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page")
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    print(vars(rooms.paginator))
-    return render(request, "rooms/home.html", {"rooms": rooms})
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+    def room_detail(request, pk):
+        try:
+            room = models.Room.objects.get(pk=pk)
+            return render(request, "rooms/detail.html", {"room": room})
+        except models.Room.DoesNotExist:
+            raise Http404()
